291ac636221fa40d1723da66f8b13c8856e79aad
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2022-11-13 23:33:47 +0530

    feat: update default terminal to kitty

--- config.def.h
+++ config.def.h
@@ -13,7 +13,7 @@ static const int showsystray        = 1;        /* 0 means no systray */
 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Noto Sans Mono:size=8" };
+static const char *fonts[]          = { "Noto Sans Mono:size=8", "Material Design Icons Desktop:size=8" };
 static const char dmenufont[]       = "Noto Sans Mono:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -74,7 +74,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_peach, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", "-e", "tmux" };
+static const char *termcmd[]  = { "kitty" };
 static const char *roficmd[]  = { "rofi", "-show", "drun", NULL};
 
 static Key keys[] = {
